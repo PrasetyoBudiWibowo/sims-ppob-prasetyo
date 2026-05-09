65451ed5f92cccff87c12683c69c180d4d606573
Prasetyo Budi <prasetyob384@gmail.com>
2026-05-09 11:29:33 +0700

    fix SPA routing railway

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  publicDir: "public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
